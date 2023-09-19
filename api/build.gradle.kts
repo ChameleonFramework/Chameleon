ChameleonFramework/Chameleon

commit 21ec9eb6a450497a8f1b9ad8f7ea8c52e0e30be5

fix(annotations): fix velocity generation

platform-bukkit/build.gradle.kts
@@ -31,7 +31,7 @@ repositories {
 
 dependencies {
     api(project(":chameleon-core"))
-    compileOnly(libs.platform.bukkit)
+    compileOnlyApi(libs.platform.bukkit)
     implementation(libs.adventure.platform.bukkit)
     compileOnlyApi(libs.annotations)
 }

platform-bungeecord/build.gradle.kts
@@ -33,7 +33,7 @@ repositories {
 
 dependencies {
     api(project(":chameleon-core"))
-    compileOnly(libs.platform.bungeecord)
+    compileOnlyApi(libs.platform.bungeecord)
     implementation(libs.adventure.platform.bungeecord)
     compileOnlyApi(libs.annotations)
 }

platform-velocity/build.gradle.kts
@@ -30,6 +30,6 @@ repositories {
 
 dependencies {
     api(project(":chameleon-core"))
-    compileOnly(libs.platform.velocity)
+    compileOnlyApi(libs.platform.velocity)
     compileOnlyApi(libs.annotations)
 }

platform-minestom/build.gradle.kts
@@ -32,6 +32,6 @@ repositories {
 
 dependencies {
     api(project(":chameleon-core"))
-    compileOnly(libs.platform.minestom)
+    compileOnlyApi(libs.platform.minestom)
     compileOnlyApi(libs.annotations)
 }

annotations/src/main/java/dev/hypera/chameleon/annotations/processing/generation/impl/velocity/VelocityGenerator.java
@@ -120,7 +120,7 @@ public class VelocityGenerator extends Generator {
         for (PlatformDependency dependency : data.dependencies()) {
             AnnotationSpec dependencyAnnotationSpec = AnnotationSpec.builder(clazz("com.velocitypowered.api.plugin", "Dependency")).addMember("id", "$S", dependency.name().toLowerCase()).addMember("optional", "$L", dependency.soft()).build();
 
-            pluginAnnotationSpecBuilder.addMember("authors", "$L", dependencyAnnotationSpec);
+            pluginAnnotationSpecBuilder.addMember("dependencies", "$L", dependencyAnnotationSpec);
         }
 
         TypeSpec velocityMainClassSpec = TypeSpec.classBuilder(plugin.getSimpleName() + "Velocity")

commit 6f002013189936474c3876f700535e4b4ed9e8bc

build!: fix dependencies (#289)
Fix Gradle dependency problem (@joshuasing PEBKAC) introduced in
https://github.com/ChameleonFramework/Chameleon/commit/21ec9eb6a450497a8f1b9ad8f7ea8c52e0e30be5
`compileOnlyApi` results in the Maven scope `compile` which causes the
dependency to be included by the Maven shade plugin.

Use `compileOnly` for platform dependencies (e.g. Bukkit, BungeeCord).
Use `api` for dependencies that should be transitvely exported to
consumers (e.g. chameleon-api, adventure).

More information:
https://docs.gradle.org/current/userguide/java_library_plugin.html

api/build.gradle.kts
@@ -31,7 +31,9 @@ dependencies {
     api(libs.adventure.textSerializer.legacy)
     api(libs.adventure.textSerializer.gson)
     api(libs.adventure.platform.api)
+    compileOnlyApi(libs.annotations)
+
+    // Logging libraries for logger wrappers
     compileOnly(libs.slf4j)
     compileOnly(libs.log4j) // Scary...
-    compileOnlyApi(libs.annotations)
 }

example/build.gradle.kts
@@ -51,14 +51,34 @@ repositories {
 }
 
 dependencies {
+    // Chameleon API
     implementation(projects.chameleonApi) // dev.hypera:chameleon-api
-    implementation(projects.chameleonPlatformBukkit) // dev.hypera:chameleon-platform-bukkit
-    implementation(projects.chameleonPlatformBungeecord) // dev.hypera:chameleon-platform-bungeecord
-    implementation(projects.chameleonPlatformFolia) // dev.hypera:chameleon-platform-folia
-    implementation(projects.chameleonPlatformNukkit) // dev.hypera:chameleon-platform-nukkit
-    implementation(projects.chameleonPlatformVelocity) // dev.hypera:chameleon-platform-velocity
-    implementation(projects.chameleonPlatformSponge) // dev.hypera:chameleon-platform-sponge
 
+    // Bukkit support
+    implementation(projects.chameleonPlatformBukkit) // dev.hypera:chameleon-platform-bukkit
+    compileOnly(libs.platform.bukkit) // org.spigotmc:spigot-api
+
+    // BungeeCord support
+    implementation(projects.chameleonPlatformBungeecord) // dev.hypera:chameleon-platform-bungeecord
+    compileOnly(libs.platform.bungeecord) // net.md-5:bungeecord-api
+
+    // Folia support
+    implementation(projects.chameleonPlatformFolia) // dev.hypera:chameleon-platform-folia
+    compileOnly(libs.platform.folia) // dev.folia:folia-api
+
+    // Nukkit support
+    implementation(projects.chameleonPlatformNukkit) // dev.hypera:chameleon-platform-nukkit
+    compileOnly(libs.platform.nukkit) // cn.nukkit:nukkit
+
+    // Sponge support
+    implementation(projects.chameleonPlatformSponge) // dev.hypera:chameleon-platform-sponge
+    compileOnly(libs.platform.sponge) // org.spongepowered:spongeapi
+
+    // Velocity support
+    implementation(projects.chameleonPlatformVelocity) // dev.hypera:chameleon-platform-velocity
+    compileOnly(libs.platform.velocity) // com.velocitypowered:velocity-api
+
+    // Annotation-based platform "main class" and manifest generation
     compileOnly(projects.chameleonAnnotations) // dev.hypera:chameleon-annotations
     annotationProcessor(projects.chameleonAnnotations) // dev.hypera:chameleon-annotations
 }

platform-api/build.gradle.kts
@@ -27,5 +27,5 @@ plugins {
 }
 
 dependencies {
-    compileOnlyApi(projects.chameleonApi)
+    api(projects.chameleonApi)
 }

platform-bukkit/build.gradle.kts
@@ -31,8 +31,7 @@ repositories {
 }
 
 dependencies {
-    compileOnlyApi(projects.chameleonApi)
-    compileOnlyApi(projects.chameleonPlatformApi)
-    compileOnlyApi(libs.platform.bukkit)
+    api(projects.chameleonPlatformApi)
+    compileOnly(libs.platform.bukkit)
     implementation(libs.adventure.platform.bukkit)
 }

platform-bungeecord/build.gradle.kts
@@ -31,8 +31,7 @@ repositories {
 }
 
 dependencies {
-    compileOnlyApi(projects.chameleonApi)
-    compileOnlyApi(projects.chameleonPlatformApi)
-    compileOnlyApi(libs.platform.bungeecord)
+    api(projects.chameleonPlatformApi)
+    compileOnly(libs.platform.bungeecord)
     implementation(libs.adventure.platform.bungeecord)
 }

platform-folia/build.gradle.kts
@@ -35,5 +35,5 @@ dependencies {
     api(projects.chameleonPlatformBukkit) {
         exclude("org.spigotmc", "spigot-api")
     }
-    compileOnlyApi(libs.platform.folia)
+    compileOnly(libs.platform.folia)
 }

platform-nukkit/build.gradle.kts
@@ -31,7 +31,6 @@ repositories {
 }
 
 dependencies {
-    compileOnlyApi(projects.chameleonApi)
-    compileOnlyApi(projects.chameleonPlatformApi)
-    compileOnlyApi(libs.platform.nukkit)
+    api(projects.chameleonPlatformApi)
+    compileOnly(libs.platform.nukkit)
 }

platform-sponge/build.gradle.kts
@@ -31,9 +31,8 @@ repositories {
 }
 
 dependencies {
-    compileOnlyApi(projects.chameleonApi)
-    compileOnlyApi(projects.chameleonPlatformApi)
-    compileOnlyApi(libs.platform.sponge) {
+    api(projects.chameleonPlatformApi)
+    compileOnly(libs.platform.sponge) {
         exclude(module = "configurate-*")
     }
 }

platform-velocity/build.gradle.kts
@@ -31,7 +31,6 @@ repositories {
 }
 
 dependencies {
-    compileOnlyApi(projects.chameleonApi)
-    compileOnlyApi(projects.chameleonPlatformApi)
-    compileOnlyApi(libs.platform.velocity)
+    api(projects.chameleonPlatformApi)
+    compileOnly(libs.platform.velocity)
 }